86cc41af7637fa163004a4a3e09988f483e3eed8
Zachary Hafen <zachary.h.hafen@gmail.com>
2022-07-25 21:50:05 -0700

    Added basic analysis up to calculating the sightline coordinates.

---
+++ analysis/helpers.py
@@ -0,0 +1,6 @@
+PROJECT_NAME = 'mock-MW-CGM-observations'
+
+# Stampede2 config file
+# CONFIG = '/home1/03057/zhafen/papers/{}/analysis/analysis.trove'.format( PROJECT_NAME )
+# Local config file
+CONFIG = '/Users/zhafen/paper_repos/{}/analysis/analysis.trove'.format( PROJECT_NAME )
